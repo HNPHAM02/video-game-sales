16639b80a506c126456d1d373fc6c1a3da8e9509
Hung Pham <hnpham02@gmail.com>
2025-11-30 14:19:53 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -87,6 +87,7 @@ def get_sales():
     # finish sql
     sql += """
         ORDER BY s.salesValue DESC
+        LIMIT 20;
     """
 
     cur.execute(sql, params)
@@ -159,6 +160,7 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
